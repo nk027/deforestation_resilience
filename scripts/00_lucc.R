bafbe594679373362ba1ff7fac3e64a93385ec00
Nikolas Kuschnig <nkuschni@wu.ac.at>
2022-11-10 13:23:04 +0100

    Clean up a bit

--- scripts/00_lucc.R
+++ scripts/00_lucc.R
@@ -5,57 +5,60 @@ library("readxl")
 library("readr")
 library("tidyr")
 
+# The data source is "COBERTURA E TRANSIÇÕES MUNICÍPIOS (COLEÇÃO 7)",
+# Version 7, August 2022, from <https://mapbiomas.org/estatisticas>
+
 # Land cover in hectare ---
 d_luc <- readxl::read_excel("data/land_use/land-cover-municipio_collection7.xlsx",
-                            sheet = "COBERTURA_COL7") |>
+  sheet = "COBERTURA_COL7") |>
   select(id = feature_id, muni = municipality, state = UF, biome, class_id,
-         class = level_2, subclass = level_3, color, `2000`:`2021`) |>
+    class = level_2, subclass = level_3, color, `2000`:`2021`) |>
   tidyr::pivot_longer(cols = `2000`:`2021`, names_to = "year") |>
   mutate(year = as.integer(year))
 
 # Land transitions in hectare ---
-# XLS is inefficient in terms of RAM, so I export the sheet manually to CSV instead
+# XLS crashes due to lack of RAM (>16GB), so I exported "TRANSICAO_COL7" manually as CSV instead
 d_lut <- readr::read_csv("data/land_use/land-cover-municipio_collection7.csv",
-                         col_types = "i---iccccccii--------------------------n---n-n-nnn-nnn--nn---nn-nnn-n-nnnn") |>
+  col_types = "i---iccccccii--------------------------n---n-n-nnn-nnn--nn---nn-nnn-n-nnnn") |>
   transmute(id = feature_id, class_id, class = as.factor(level_2),
-            subclass = as.factor(level_3), color = as.factor(color),
-            from = from_class, to = to_class, `2000-2001`, `2001-2002`, `2002-2003`,
-            `2003-2004`, `2004-2005`, `2005-2006`, `2006-2007`, `2007-2008`, `2008-2009`,
-            `2009-2010`, `2010-2011`, `2011-2012`, `2012-2013`, `2013-2014`, `2014-2015`,
-            `2015-2016`, `2016-2017`, `2017-2018`, `2018-2019`, `2019-2020`, `2020-2021`) |>
+    subclass = as.factor(level_3), color = as.factor(color),
+    from = from_class, to = to_class, `2000-2001`, `2001-2002`, `2002-2003`,
+    `2003-2004`, `2004-2005`, `2005-2006`, `2006-2007`, `2007-2008`, `2008-2009`,
+    `2009-2010`, `2010-2011`, `2011-2012`, `2012-2013`, `2013-2014`, `2014-2015`,
+    `2015-2016`, `2016-2017`, `2017-2018`, `2018-2019`, `2019-2020`, `2020-2021`) |>
   tidyr::pivot_longer(cols = `2000-2001`:`2020-2021`, names_to = "year") |>
   mutate(year = as.integer(substr(year, 6, 9)))
 
-# Prepare to merge the cover and change and get to a wide format ---
-d_luc <- d_luc |> filter(subclass %in% c("Forest Loss", "Forest Formation", "Pasture", # Keep these
-                                         "Temporary Crops", "Perennial Crops", "Urban Infrastructure", "Mining", "Forest Plantation")) |>
+# Prepare to merge the cover and change and adjust to a wide format ---
+d_luc <- d_luc |> filter(subclass %in% # We keep these
+  c("Forest Loss", "Forest Formation", "Pasture", "Temporary Crops",
+  "Perennial Crops", "Urban Infrastructure", "Mining", "Forest Plantation")) |>
   select(id, muni, state, biome, year, subclass, value) |>
   tidyr::pivot_wider(names_from = subclass, values_from = value,
-                     values_fn = \(x) sum(x, na.rm = TRUE), values_fill = 0) |>
+    values_fn = \(x) sum(x, na.rm = TRUE), values_fill = 0) |>
   transmute(id, muni, state, biome, year,
-            forest = `Forest Formation`, forestry = `Forest Plantation`,
-            croplands = `Temporary Crops` + `Perennial Crops`, pasture = `Pasture`,
-            urban = `Urban Infrastructure`, mining = `Mining`)
+    forest = `Forest Formation`, forestry = `Forest Plantation`,
+    croplands = `Temporary Crops` + `Perennial Crops`, pasture = `Pasture`,
+    urban = `Urban Infrastructure`, mining = `Mining`)
 d_lut <- d_lut |> filter(from == 3, to != 3) |>  group_by(id, year) |>
   summarise(forest_loss = sum(value, na.rm = TRUE))
 d_lucc <- d_luc |> left_join(d_lut, by = c("year", "id")) |>
   tidyr::replace_na(list(forest_loss = 0))
 
-# We care about the Amazon and Cerrado (and a bit of Pantanal) ---
+# We care about the Legal Amazon, i.e. Amazon, Cerrado, and a bit of Pantanal
 d_lucc <- d_lucc |> mutate(biome = substr(tolower(biome), 1, 3)) |>
   filter(biome %in% c("ama", "cer", "pan"))
 
-# Now we want one row per municipality & year, first ignoring biomes
+# We want one row per municipality & year, first ignoring biomes
 d_lucc_all <- d_lucc |> group_by(muni, state, year) |>
   summarise(across(forest:forest_loss, sum, na.rm = TRUE))
 
-# Next, we keep the Amazon specific columns
+# Next, we add back the Amazon-bome specific columns
 d_lucc_biomes <- d_lucc |> select(-id) |> filter(biome == "ama") |>
   tidyr::pivot_wider(names_from = biome, values_from = forest:forest_loss,
-                     values_fn = \(x) sum(x, na.rm = TRUE), values_fill = 0)
-
+    values_fn = \(x) sum(x, na.rm = TRUE), values_fill = 0)
 d_lucc <- d_lucc_all |> left_join(d_lucc_biomes, by = c("muni", "state", "year")) |>
   mutate(across(forest_ama:forest_loss_ama, ~ ifelse(is.na(.), 0, .)))
 
+# Save the data
 saveRDS(d_lucc, "data/land_use_change.rds")
-d_lucc <- readRDS("data/land_use_change.rds")
--- scripts/00_spatial.R
+++ scripts/00_spatial.R
@@ -4,105 +4,94 @@ library("sf")
 library("dplyr")
 
 states <- c("AC" = "Acre", "AL" = "Alagoas", "AP" = "Amapá", "AM" = "Amazonas",
-            "BA" = "Bahia", "CE" = "Ceará", "DF" = "Distrito Federal", "ES" = "Espírito Santo",
-            "GO" = "Goiás", "MA" = "Maranhão", "MT" = "Mato Grosso", "MS" = "Mato Grosso do Sul",
-            "MG" = "Minas Gerais", "PA" = "Pará", "PB" = "Paraíba", "PR" = "Paraná",
-            "PE" = "Pernambuco", "PI" = "Piauí", "RJ" = "Rio de Janeiro", "RN" = "Rio Grande do Norte",
-            "RS" = "Rio Grande do Sul", "RO" = "Rondônia", "RR" = "Roraima", "SC" = "Santa Catarina",
-            "SP" = "São Paulo", "SE" = "Sergipe", "TO" = "Tocantins")
+  "BA" = "Bahia", "CE" = "Ceará", "DF" = "Distrito Federal", "ES" = "Espírito Santo",
+  "GO" = "Goiás", "MA" = "Maranhão", "MT" = "Mato Grosso", "MS" = "Mato Grosso do Sul",
+  "MG" = "Minas Gerais", "PA" = "Pará", "PB" = "Paraíba", "PR" = "Paraná",
+  "PE" = "Pernambuco", "PI" = "Piauí", "RJ" = "Rio de Janeiro", "RN" = "Rio Grande do Norte",
+  "RS" = "Rio Grande do Sul", "RO" = "Rondônia", "RR" = "Roraima", "SC" = "Santa Catarina",
+  "SP" = "São Paulo", "SE" = "Sergipe", "TO" = "Tocantins")
 ufs <- structure(names(states), names = states)
 legal_amazon <- c("AC", "AP", "AM",  "MA", "MT", "PA", "RO", "RR", "TO")
 
-
-# Plain shapefile ---
+# Plain shapefile with municipalities ---
 sh <- read_sf("data/shp") |>
   transmute(muni_id = as.integer(CD_MUN), muni = NM_MUN, state = SIGLA, area_km2 = AREA_KM2)
-
 saveRDS(sh |> st_drop_geometry(), "data/spatial.rds")
 
 # Protected areas ---
-pa <- rbind(read_sf("data/protected_areas/WDPA_WDOECM_Sep2022_Public_BRA_shp_0"),
-            read_sf("data/protected_areas/WDPA_WDOECM_Sep2022_Public_BRA_shp_1"),
-            read_sf("data/protected_areas/WDPA_WDOECM_Sep2022_Public_BRA_shp_2")) |>
+
+# Data from Protected Planet at <https://www.protectedplanet.net>
+pa <- rbind(
+  read_sf("data/protected_areas/WDPA_WDOECM_Sep2022_Public_BRA_shp_0"),
+  read_sf("data/protected_areas/WDPA_WDOECM_Sep2022_Public_BRA_shp_1"),
+  read_sf("data/protected_areas/WDPA_WDOECM_Sep2022_Public_BRA_shp_2")) |>
   filter(STATUS != "Proposed", MARINE == "0") |>
   transmute(name = NAME, designation = DESIG_ENG, type = GOV_TYPE,
-            year = as.integer(pmax(1926, STATUS_YR)), pa_area_km2 = REP_AREA) |> # One's designated by Jesus
+    # One was designated by the Romans, we cap it at the interwar period
+    year = as.integer(pmax(1926, STATUS_YR)), pa_area_km2 = REP_AREA) |>
   st_transform(st_crs(sh))
-
 pa_amazon <- pa |> st_intersection(sh |> filter(state %in% legal_amazon) |> st_bbox() |> st_as_sfc())
-
 pa_int <- sh |> filter(state %in% legal_amazon) |> st_intersection(pa_amazon)
 
-saveRDS(pa_int, "data/pa_raw.rds")
-pa_int <- readRDS("data/pa_raw.rds")
-
+# Add some information
 pa_int <- pa_int |> rename(pa_total_area_km2 = pa_area_km2) |>
-  mutate(pa_area_km2 = as.numeric(st_area(pa_int) / 1e6), 
-         pa_area_km2 = ifelse(pa_area_km2 > area_km2, area_km2, pa_area_km2)) |>
+  mutate(pa_area_km2 = as.numeric(st_area(pa_int) / 1e6),
+    pa_area_km2 = ifelse(pa_area_km2 > area_km2, area_km2, pa_area_km2)) |>
   rename(pa_year = year, pa_type = type, pa_designation = designation, pa_name = name)
 
-
+# Save the protected areas
 saveRDS(pa_int, "data/pa.rds")
 pa_int <- readRDS("data/pa.rds")
 
-
-pa_df <- pa_int |> 
-  st_drop_geometry() |> 
-  select(-pa_name, -pa_designation, -pa_total_area_km2) |> 
-  group_by(muni_id, muni, state, area_km2, pa_type, pa_year) |> 
-  summarise(pa_area_km2 = sum(pa_area_km2, na.rm = TRUE)) |> 
+pa_df <- pa_int |>
+  st_drop_geometry() |>
+  select(-pa_name, -pa_designation, -pa_total_area_km2) |>
+  group_by(muni_id, muni, state, area_km2, pa_type, pa_year) |>
+  summarise(pa_area_km2 = sum(pa_area_km2, na.rm = TRUE)) |>
   tidyr::pivot_wider(names_from = pa_type, values_from = pa_area_km2) |>
-  rename(pa_fed_km2 = `Federal or national ministry or agency`, 
-         pa_ind_km2 = `Indigenous peoples`, 
-         pa_oth_km2 = `Not Reported`, 
-         pa_sub_km2 = `Sub-national ministry or agency`, 
-         pa_loc_km2 = `Local communities`) |> 
+  rename(pa_fed_km2 = `Federal or national ministry or agency`,
+    pa_ind_km2 = `Indigenous peoples`, pa_oth_km2 = `Not Reported`,
+    pa_sub_km2 = `Sub-national ministry or agency`,
+    pa_loc_km2 = `Local communities`) |>
   ungroup()
 
-
-pa_int <- pa_int |> 
-  arrange(muni_id, pa_year)
+pa_int <- pa_int |> arrange(muni_id, pa_year)
 pa_tot <- c()
 for(m_id in unique(pa_int$muni_id)) {
-  temp <- pa_int |> 
-    filter(muni_id == m_id)
+  temp <- pa_int |> filter(muni_id == m_id)
   if(nrow(temp) > 1) {
-    for(yy in 2:nrow(temp)) {
+    for(yy in seq(2, nrow(temp))) {
       temp[yy, "geometry"] <- st_make_valid(st_set_precision(
-        st_union(temp[yy - 1, "geometry"], temp[yy, "geometry"]), 
-        1e6))
-    }  
+        st_union(temp[yy - 1, "geometry"], temp[yy, "geometry"]), 1e6))
+    }
   }
-  temp <- temp |> 
-    mutate(pa_area_km2 = as.numeric(st_area(temp) / 1e6)) |> 
+  temp <- temp |>
+    mutate(pa_area_km2 = as.numeric(st_area(temp) / 1e6)) |>
     st_drop_geometry()
   pa_tot <- rbind(pa_tot, temp)
 }
 
-pa_tot <- pa_tot |> 
-  arrange(muni_id, pa_year, pa_area_km2) |> 
-  group_by(muni_id, muni, state, area_km2, pa_year) |> 
-  slice_tail(n = 1) |> 
-  ungroup() |> 
+pa_tot <- pa_tot |>
+  arrange(muni_id, pa_year, pa_area_km2) |>
+  group_by(muni_id, muni, state, area_km2, pa_year) |>
+  slice_tail(n = 1) |>
+  ungroup() |>
   transmute(muni_id, muni, state, pa_year,
-            pa_tot_km2 =  ifelse(pa_area_km2 > area_km2, area_km2, pa_area_km2)) 
-
+    pa_tot_km2 =  ifelse(pa_area_km2 > area_km2, area_km2, pa_area_km2))
 pa_df <- left_join(pa_df, pa_tot, by = c("muni_id", "muni", "state", "pa_year"))
 
-muni_grid <- expand.grid("muni_id" = unique(sh$muni_id), "pa_year" = min(pa_df$pa_year):2020) |> 
-  left_join(sh |> st_drop_geometry() |> select(-area_km2), by = "muni_id") |> 
+muni_grid <- expand.grid("muni_id" = unique(sh$muni_id), "pa_year" = seq(min(pa_df$pa_year), 2020)) |>
+  left_join(sh |> st_drop_geometry() |> select(-area_km2), by = "muni_id") |>
   relocate(pa_year, .after = state)
 
-pa_df <- left_join(muni_grid, pa_df |> select(-muni, -state, -area_km2), by = c("muni_id", "pa_year")) |> 
-  tidyr::replace_na(list(pa_fed_km2 = 0, pa_ind_km2 = 0, pa_oth_km2 = 0, 
-                         pa_sub_km2 = 0, pa_loc_km2 = 0)) |> 
-  arrange(muni_id, pa_year) |> 
-  group_by(muni_id) |> 
-  mutate(across(pa_fed_km2:pa_loc_km2, ~cumsum(.x)),
-         pa_tot_km2 = zoo::na.locf(pa_tot_km2, na.rm = FALSE)) |> 
-  tidyr::replace_na(list(pa_tot_km2 = 0)) |> 
-  ungroup() |> 
+pa_df <- left_join(muni_grid, pa_df |> select(-muni, -state, -area_km2), by = c("muni_id", "pa_year")) |>
+  tidyr::replace_na(list(pa_fed_km2 = 0, pa_ind_km2 = 0, pa_oth_km2 = 0, pa_sub_km2 = 0, pa_loc_km2 = 0)) |>
+  arrange(muni_id, pa_year) |>
+  group_by(muni_id) |>
+  mutate(across(pa_fed_km2:pa_loc_km2, ~cumsum(.x)), pa_tot_km2 = zoo::na.locf(pa_tot_km2, na.rm = FALSE)) |>
+  tidyr::replace_na(list(pa_tot_km2 = 0)) |>
+  ungroup() |>
   rename(year = pa_year)
 
+# Save a dataframe to work with
 saveRDS(pa_df, "data/pa_df.rds")
-pa_df <- readRDS("data/pa_df.rds")
